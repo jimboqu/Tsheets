98b1bd98b11757948a7a9c21c4e11c5a6db61e30
jimboqu <jamesquilter308@gmail.com>
2016-06-16 13:50:57 +0100

    added about and static controller

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,7 @@
+class StaticpagesController < ApplicationController
+  def about
+  end
+
+  def do
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
  
 
+  get 'staticpages/about'
+
+  get 'staticpages/do'
+
   devise_for :users
   resources :clients
   resources :blocks
